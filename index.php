56c8dffd9d695cf6a3021806e2b55e021b1d4d96
Mike <ma11en@hotmail.co.uk>
2019-05-30 23:17:05 +0100

    Favicon added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/main.css">
     <link href="https://fonts.googleapis.com/css?family=EB+Garamond:700|Oswald:300,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link href="assets/icons/favicon.png" rel="icon" type="image/x-icon" />
     <title>Mike Allen Web Development</title>
 </head>
 <body>
